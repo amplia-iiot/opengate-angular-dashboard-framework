5af23327571bac86053834d742da7ebd58231ed6
kmlamplia <katherin.moscoso@amplia.es>
2018-09-17 14:51:14 +0200

    OUW-1672

--- dist/opengate-angular-dashboard-framework.js
+++ dist/opengate-angular-dashboard-framework.js
@@ -28,7 +28,7 @@
 angular.module('adf', ['adf.provider', 'ui.bootstrap', 'opengate-angular-js'])
     .value('adfTemplatePath', '../src/templates/')
     .value('columnTemplate', '<adf-dashboard-column column="column" adf-model="adfModel" options="options" edit-mode="editMode" ng-repeat="column in row.columns" />')
-    .value('adfVersion', '7.1.0');
+    .value('adfVersion', '7.1.1');
 /*
  * The MIT License
  *
